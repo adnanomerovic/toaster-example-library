1c5dd1be1caf5ac9dd188727f03965e0e3d6d39e
adnanomerovic <1812992171632>
2021-04-22 10:13:57 +0200

    Preparing lib for maven

--- ToasterLibrary/src/main/java/ba/adnan/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/ba/adnan/toasterlibrary/ToasterMessage.java
@@ -1,7 +1,13 @@
 package ba.adnan.toasterlibrary;
 
+import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+
 
 public class ToasterMessage {
     public static void toastShortMessage(Context c, String message) {
@@ -10,5 +16,14 @@ public class ToasterMessage {
 
     public static void toastLongMessage(Context c, String message) {
         Toast.makeText(c, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static Completable delayToastMessage(Activity activity, String message, long delay, TimeUnit timeunit) {
+        return Completable.complete().delay(delay, timeunit).andThen(
+                Completable.fromAction(() -> {
+                    activity.runOnUiThread(() -> ToasterMessage.toastShortMessage(activity, message));
+                })
+        );
+
     }
 }
--- app/src/main/java/ba/adnan/toasterexample/MainActivity.java
+++ app/src/main/java/ba/adnan/toasterexample/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
 import ba.adnan.toasterlibrary.ToasterMessage;
+import io.reactivex.Completable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ToasterMessage.toastShortMessage(getApplicationContext(),"Library works fine!");
+        ToasterMessage.toastShortMessage(getApplicationContext(), "Library works fine!");
+
+        ToasterMessage.delayToastMessage(
+                MainActivity.this,
+                "Delay toast",
+                5,
+                TimeUnit.SECONDS
+        ).subscribe();
+
+        Completable.complete().delay(3, TimeUnit.SECONDS).andThen(
+                Completable.fromAction(() -> {
+                    runOnUiThread(() -> ToasterMessage.toastShortMessage(getApplicationContext(), "message"));
+                })).subscribe();
+
     }
 }
